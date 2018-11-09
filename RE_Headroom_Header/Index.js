8e2e4f711e1bdf05ba0c716361772822f7bacf3f
GUZHAO <guzhao1990@gmail.com>
2018-11-08 23:25:48 -0500

    feat: embed tableau dashboard
    Description: embed tableau dashboard for testing
    
    Status: added, but the api is a bit strange to me.

--- RE_Headroom_Header/Index.js
+++ RE_Headroom_Header/Index.js
@@ -1,1 +1,23 @@
 $(".d-flex").headroom();
+
+
+function initViz() {
+    var containerDiv = document.getElementById("vizContainer"),
+        url = "http://dfcitableau.partners.org/#/site/POPC/views/POPCOutpatient/Overview?:iid=85";
+
+    var viz = new tableau.Viz(containerDiv, url);
+}
+
+initViz();
+//
+// const tabi = document.getElementById('popc').contentWindow;
+// // // // const innerdoc = iframe.contentDocument;
+// // const ift = tabi.children[0];
+// console.log(tabi.contentDocument);
+// const test = innerdoc.getElementById('tab-container');
+// test.addEventListener('click', () => {
+//     alert('I see you');
+// });
+
+
+// tippy(document.querySelector('#tab-container'), {content: "I'm a tooltip!"});
